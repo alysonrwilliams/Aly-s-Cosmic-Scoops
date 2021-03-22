f255c8e924fe241bec3804786477a2b586513b19
alyson williams <alysonrwilliams@gmail.com>
2021-03-22 18:28:24 -0400

    added JS function that returns user name in alert box

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* This function switches the nav menus */
 function myFunction() {
   var x = document.getElementById("navLinks");
   if (x.style.display === "block") {
@@ -8,6 +9,17 @@ function myFunction() {
 }
 
 
+/* This function validates the first name field on the form */
+function validateForm() {
+  var x = document.forms["theForm"]["firstname"].value;
+  if (x == "") {
+    alert("Oops! You didn't tell us your name!");
+    return false;
+  }
+}
 
-
-
+/* This function prompts an alert to the customer after submitting */
+function myfunction() {
+var x=document.getElementById('fname').value;
+alert("Hello, " +x + "! Your ice cream is being prepared!");
+}
