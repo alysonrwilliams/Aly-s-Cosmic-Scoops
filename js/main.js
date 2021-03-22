cd6d9761755b44af7ca26154d5a16c8d89ead1f8
alyson williams <alysonrwilliams@gmail.com>
2021-03-22 16:21:50 -0400

    added mobile nav bar and some cosmetic changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+function myFunction() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+
 
 
