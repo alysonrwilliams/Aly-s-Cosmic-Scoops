31de7ef22893d2f69f6b57a277475f5c0eae15e6
Alyson Williams <alysonrwilliams@gmail.com>
2021-07-26 20:28:24 -0400

    adding fetch function for the NASA API

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ function myFunction() {
   }
 }
 
+/*This is the Random Desert Generator */
+function randomDesertGenerator() {
+const deserts = [
+    "Lemon Blueberry Galaxy", 
+    "The Milky Way", 
+    "Sprinkled Stardust", 
+    "Oreo Moon Rock Shake", 
+    "The Zero Gravity"
+];
+    
+    const random = deserts[Math.random() * deserts.length | 0]
+    alert("We think you should order the " + random, deserts[random]);
+}
 
 
 /* This function validates the name field + prompts alert to customer on submit */
@@ -21,13 +34,54 @@ alert("Thanks, " +x + "! Your ice cream is being prepared!");
 }
 }
 
+/* This is the Tip Calculator Function*/
+function tipCalculator (slider, bill){
+  var tip = document.getElementById('tipamount');
+  var slideval = document.getElementById('slideval');
+  var bill = document.getElementById(bill).value;
+  var prcnt = slider * .01;
+  
+  
+  if (bill == null || bill == '') {
+    tip.innerHTML = 'Oops! Enter an amount.';
+    return false;
+  }
+  if(isNaN(bill)) {
+    tip.innerHTML = 'Oops! Make sure you entered a real number.';
+    return false;
+  }
+  if(bill >= 0){
+    tip.innerHTML = '$' + (bill * prcnt) .toFixed(2);
+  slideval.innerHTML = slider + '%';
+  }
+}
 
-/* Tip Calculator Function*/
-function calc() {
-        var bill = Number(document.getElementById('bill').value);
-        var tip = bill * .20;
-        var total_bill = bill + tip;
 
-        document.getElementById("tip").innerHTML= "$"+Number(tip).toFixed(2);
-        document.getElementById("total").innerHTML= "$"+Number(total_bill).toFixed(2);         
-    }
+/* This is fetching the NASA API */
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch(`${url}${api_key}`)
+    const data = await response.json()
+    console.log('NASA APOD data', data)
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/*This is displaying the NASA image on the site */
+
+const displayData = data => {
+  document.getElementById('title').textContent = data.title
+  document.getElementById('date').textContent = data.date
+  document.getElementById('picture').src = data.hdurl
+  document.getElementById('explanation').textContent = data.explanation
+}
+
+fetchNASAData()
+
+fetchNASAData()
